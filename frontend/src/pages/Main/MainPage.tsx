bb8b5bd3bbafc3db24bfa26a6b5715b531dad3d6
makecsx <lex200900@gmail.com>
2020-11-28 20:25:58 +0300

    frontend (Styling): finalize mainPage markup

--- frontend/src/pages/Main/MainPage.tsx
+++ frontend/src/pages/Main/MainPage.tsx
@@ -3,6 +3,7 @@ import styles from './MainPage.module.scss';
 import classNames from "classnames";
 import Grid from '@material-ui/core/Grid';
 import backgroundGreenGradient from '../../assets/images/background-green-gradient.png';
+import { Icon, CircularProgress } from '@material-ui/core';
 
 const MainPage = () => {
     const cx = classNames.bind(styles);
@@ -10,7 +11,7 @@ const MainPage = () => {
     return (
         <Grid className={cx(styles.container)} container
         >
-            <Grid className={cx(styles.containerItem)} item sm={6}>
+            <Grid className={cx(styles.containerItem)} item xs={7}>
                 <div className={cx(styles.leftItemContainer)}>
                     <div>
                         <h1>Invenient<br/> Face</h1>
@@ -22,29 +23,43 @@ const MainPage = () => {
                 background: `url(${backgroundGreenGradient})`,
                 backgroundPosition: 'center',
                 backgroundSize: 'cover'
-            }} className={cx(styles.containerItem)} item sm={6}>
+            }} className={cx(styles.containerItem)} item xs={5}>
                 <div className={cx(styles.rightItemContainer)}>
-                    <p className={cx(styles.rightItemTitle)}>
+                    <p className={cx(styles.itemTitle)}>
                         Сервис для идентификации по фотографии клиентов
                         банка и сбора их финансового профиля из различных открытых источников.
                     </p>
                     <Grid className={cx(styles.progressStepsContainer)} container alignItems="center">
-                        <Grid xs={2}>
+                        <Grid item xs={2}>
                             <span className={cx(styles.progressStep)}>1</span>
                         </Grid>
-                        <Grid className={cx(styles.progressMessage)} xs={8}>
+                        <Grid item className={cx(styles.progressMessage)} xs={8}>
                             Добавьте ссылки
                         </Grid>
-                       <Grid xs={2}>
+                       <Grid item xs={2}>
                            <span className={cx(styles.progressStep, styles.notActive)}>2</span>
                        </Grid>
                     </Grid>
+
+                    {/*TODO: change text input after button clicked*/}
                     <textarea rows={12} className={cx(styles.textArea)}>
-
                     </textarea>
-                    <div className={cx(styles.infoBeforeUpdload)}>
+                    <button className={cx(styles.submitLinksButton)}>
+                         ОТПРАВИТЬ
+                    </button>
 
-                    </div>
+                    {/*TODO: upload Image*/}
+                    {/*<div className={cx(styles.uploadImageButton)}>*/}
+                    {/*    /!*<Icon>*!/*/}
+                    {/*    /!*    backup*!/*/}
+                    {/*    /!*</Icon>*!/*/}
+                    {/*    */}
+                    {/*    /!*TODO: add spinner when loading*!/*/}
+                    {/*    <CircularProgress style={{color: 'white'}} size={50} />*/}
+                    {/*</div>*/}
+                    {/*<div className={cx(styles.infoBeforeUpdload)}>*/}
+
+                    {/*</div>*/}
                 </div>
             </Grid>
         </Grid>
